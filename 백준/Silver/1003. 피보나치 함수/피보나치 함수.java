1190eace5045bc2c065e6d354a6990ca7705ccd4
SungEun Kim <81363864+sung-silver@users.noreply.github.com>
2025-02-18 02:32:53 +0900

    [Silver III] Title: 피보나치 함수, Time: 124 ms, Memory: 16084 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,32 +1,48 @@
+import java.io.*;
 import java.util.*;
-public class Main {
 
-    static Scanner sc = new Scanner(System.in);
+class Fibo {
+    private int zero;
+    private int one;
 
-    static class Fibo{
-        int zeroCount=0;
-        int oneCount=0;
-        public Fibo(int zeroCount, int oneCount) {
-            this.zeroCount = zeroCount;
-            this.oneCount = oneCount;
-        }
+    public Fibo(int zero, int one) {
+        this.zero = zero;
+        this.one = one;
     }
-    public static void main(String[] args) {
-        int TC = 0;
-        int number =0;
+
+    public int getZero() {
+        return zero;
+    }
+
+    public int getOne() {
+        return one;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         Fibo[] fibos = new Fibo[41];
-        fibos[0] = new Fibo(1,0);
-        fibos[1] = new Fibo(0,1);
+        fibos[0] = new Fibo(1, 0);
+        fibos[1] = new Fibo(0, 1);
 
-        for(int i=2;i<=40;i++){
-            fibos[i] = new Fibo(fibos[i-1].zeroCount+fibos[i-2].zeroCount, fibos[i-1].oneCount+fibos[i-2].oneCount);
+        for(int i=2; i<=40; i++) {
+            int zero = fibos[i-2].getZero() + fibos[i-1].getZero();
+            int one = fibos[i-2].getOne() + fibos[i-1].getOne();
+            fibos[i] = new Fibo(zero, one);
         }
 
-        TC = sc.nextInt();
+        int N = Integer.parseInt(st.nextToken());
 
-        for(int j=0;j<TC;j++){
-            number = sc.nextInt();
-            System.out.println(fibos[number].zeroCount+" "+fibos[number].oneCount);
+        for(int i = 0; i<N ; i++) {
+            st = new StringTokenizer(br.readLine());
+            int number = Integer.parseInt(st.nextToken());
+            bw.write(fibos[number].getZero() + " " +fibos[number].getOne()+"\n");
         }
+
+        bw.flush();
+        bw.close();
     }
 }
