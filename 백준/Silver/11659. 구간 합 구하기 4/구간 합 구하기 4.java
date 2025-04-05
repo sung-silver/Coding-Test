f05ab2a07eec4df1518fd51b3b7c0e120412a4e1
SungEun Kim <81363864+sung-silver@users.noreply.github.com>
2025-04-05 09:19:17 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 660 ms, Memory: 56996 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,28 +1,35 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        int N;
-        int M;
-        int number;
-        int start;
-        int end;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int round = Integer.parseInt(st.nextToken());
+        long[] S = new long[N];
+        st = new StringTokenizer(br.readLine());
 
-        Scanner sc = new Scanner(System.in);
-
-        N = sc.nextInt();
-        M = sc.nextInt();
-
-        int[] sum = new int[N+1];
-
-        for(int i=1; i<N+1; i++){
-            sum[i]=sum[i-1]+sc.nextInt();
+        for(int i=0 ; i<N ; i++) {
+            if(i==0) {
+                S[0] = Long.parseLong(st.nextToken());
+            } else {
+                S[i] = S[i-1] + Long.parseLong(st.nextToken());
+            }
         }
-        
-        for(int j=0; j<M ; j++){
-            start = sc.nextInt();
-            end = sc.nextInt();
-            System.out.println(sum[end]-sum[start-1]);
+
+        for(int j=0 ; j<round ; j++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken()) - 1;
+            int end = Integer.parseInt(st.nextToken()) - 1;
+            if(start == 0) {
+                bw.write(S[end] + "\n");
+            } else {
+                bw.write(S[end]-S[start-1]+"\n");
+            }
         }
+        bw.flush();
+        bw.close();
     }
 }
