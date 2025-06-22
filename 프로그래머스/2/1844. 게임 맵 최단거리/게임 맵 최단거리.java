c6ad02fa4c4e63cee734e9311b7a17cc947b3f65
SungEun Kim <81363864+sung-silver@users.noreply.github.com>
2025-06-23 01:44:01 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 17.03 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,50 +1,62 @@
 import java.util.*;
 
 class Solution {
-    int[][] move = {{-1,0}, {0,1}, {1,0}, {0,-1}};
+    boolean[][] visited;
+    int[][] direction = {{1,0}, {-1,0}, {0, 1}, {0, -1}};
     
-    public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-        boolean[][] visited = new boolean[n][m];
-        return bfs(0, 0, n, m, visited, maps);
+    class Node {
+        private int x;
+        private int y;
+        private int distance;
+        
+        Node(int x, int y, int distance) {
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
+        }
+        
+        public int getX() {
+            return x;
+        }
+        
+        public int getY() {
+            return y;
+        }
+        
+        public int getDistance() {
+            return distance;
+        }
     }
     
-    public int bfs(int startX, int startY, int n, int m, boolean[][] visited, int[][] maps) {
-        int answer = -1;
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{startX, startY, 1});
-        visited[0][0] = true; // 첫번째 방문노드 초기화
+    public int solution(int[][] maps) {
+        int answer = 0;
+        visited = new boolean[maps.length][maps[0].length];
+        answer = bfs(0,0,maps);
+        return answer;
+    }
+    
+    int bfs(int x, int y, int[][] maps) {
+        Queue<Node> q = new LinkedList<>();
+        Node start = new Node(x, y, 1);
+        q.add(start);
+        visited[x][y] = true;
         
-        while(!q.isEmpty()){
-            int[] compare = q.poll();
-            int x = compare[0];
-            int y = compare[1];
-            int count = compare[2];
-            
-            boolean isTarget = x == n-1 && y == m-1;
-            if(isTarget) {
-                answer = count;
-                break;
-            }
-            
-            for(int i=0; i<4 ; i++) {
-                int nx = x + move[i][0];
-                int ny = y + move[i][1];
-                
-                boolean outOfBoard = nx < 0 || nx >= n || ny < 0 || ny >= m;
-                if(outOfBoard) continue;
-                boolean isBlocked = maps[nx][ny] == 0;
-                if(isBlocked) continue;
-                
-                boolean isNotVisted = !visited[nx][ny];
-                boolean isNotBlocked = maps[nx][ny] == 1;
-                if(isNotVisted && isNotBlocked) {
-                    visited[nx][ny] = true;
-                    q.add(new int[]{nx, ny, count+1});
+        while(!q.isEmpty()) {
+            Node current = q.poll();
+            for(int i=0 ; i<4 ; i++) {
+                int nx = current.getX() + direction[i][0];
+                int ny = current.getY() + direction[i][1];
+                if(nx < 0 || nx >= maps.length || ny < 0 || ny >= maps[0].length || visited[nx][ny] == true || maps[nx][ny] == 0) {
+                    continue;
                 }
+                if(nx == maps.length-1 && ny == maps[0].length-1) {
+                    return current.getDistance() + 1;
+                }
+                Node node = new Node(nx, ny, current.getDistance()+1);
+                q.add(node);
+                visited[nx][ny] = true;
             }
         }
-        return answer;
+        return -1;
     }
 }
