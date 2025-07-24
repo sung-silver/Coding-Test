8ff08c07fe842409e36279af4860dc04de97cbf8
SungEun Kim <81363864+sung-silver@users.noreply.github.com>
2025-07-24 10:43:45 +0900

    [level 3] Title: 네트워크, Time: 0.46 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,32 @@
+import java.util.*;
+
 class Solution {
+    // 방문 배열
     boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
         visited = new boolean[n];
-        
         for(int i=0 ; i<n ; i++) {
             if(!visited[i]) {
+                bfs(i, n, computers);
                 answer++;
-                visited[i] = true;
-                dfs(i, computers);
             }
         }
-        
         return answer;
     }
     
-    void dfs(int target, int[][] computers) {
-        for(int i = 0; i < computers.length ; i++) {
-            if(!visited[i] && computers[target][i] == 1) {
-                visited[i] = true;
-                dfs(i, computers);
+    void bfs(int start, int n, int[][] computers) {
+        Queue<Integer> q = new LinkedList<>();
+        visited[start] = true;
+        q.add(start);
+        
+        while(!q.isEmpty()) {
+            int current = q.poll();
+            for(int i=0; i<n ; i++) {
+                if(computers[current][i] == 1 && !visited[i]) {
+                    visited[i] = true;
+                    q.add(i);
+                }
             }
         }
     }
