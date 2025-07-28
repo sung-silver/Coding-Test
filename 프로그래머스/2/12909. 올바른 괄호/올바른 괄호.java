879f68e45dd91159d402a73204439a38fed85e21
SungEun Kim <81363864+sung-silver@users.noreply.github.com>
2025-07-28 10:38:16 +0900

    [level 2] Title: 올바른 괄호, Time: 21.30 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,28 +2,34 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
+        boolean answer = false;
         Stack<Character> stack = new Stack<>();
-
-        for(int i=0; i<s.length();i++){
-            char c = s.charAt(i);
-            if(c == '(') {
-                stack.push(c);
+        
+        for(int i=0 ; i<s.length() ; i++) {
+            char current = s.charAt(i);
+            if(current == '(') {
+                stack.push('(');
             }
-            else{
-                if(stack.isEmpty()||c!=')'){
+            if(current == ')' && stack.isEmpty()) {
+                answer = false;
+                break;
+            }
+            if(current == ')') {
+                boolean correct = stack.peek() == '(';
+                if(correct){
+                    stack.pop();
+                    answer = true;
+                } else {
                     answer = false;
                     break;
-                }else{
-                    stack.pop();
                 }
             }
         }
-
-        if (!stack.isEmpty()) {
+        
+        if(!stack.isEmpty()) {
             answer = false;
         }
-
+        
         return answer;
     }
 }
