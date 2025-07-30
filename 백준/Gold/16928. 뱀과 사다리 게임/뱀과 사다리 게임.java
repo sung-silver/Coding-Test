2f1c8f2f9351e0b6b8f712864d566e393a288861
SungEun Kim <81363864+sung-silver@users.noreply.github.com>
2025-07-30 17:33:50 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -1,29 +1,24 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Queue;
-import java.util.Stack;
 
 public class Main {
 
     static final int FINISH = 100;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String[] parts = br.readLine().split(" ");
         int N = Integer.parseInt(parts[0]); // 사다리
         int M = Integer.parseInt(parts[1]); // 뱀
-
         Map<Integer, Integer> teleport = new HashMap<>();
+        visited = new boolean[101];
+
         for (int i = 0; i < N + M; i++) {
             parts = br.readLine().split(" ");
             int from = Integer.parseInt(parts[0]);
@@ -31,29 +26,31 @@ public class Main {
             teleport.put(from, to);
         }
 
-        boolean[] visited = new boolean[101];
-        Queue<int[]> queue = new LinkedList<>();// [현재 위치, 이동 횟수]
-        queue.add(new int[]{1, 0});
-        visited[1] = true;
+        bfs(1, teleport);
+    }
 
+    public static void bfs(int start, Map<Integer, Integer> teleport) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{start, 0});
+        visited[start] = true;
+        
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
-            int position = current[0];
+            int index = current[0];
             int count = current[1];
 
-            if (position == FINISH) {
+            if (index == FINISH) {
                 System.out.println(count);
                 return;
             }
 
-            for (int dice = 1; dice <= 6; dice++) {
-                int next = position + dice;
-                if (next > FINISH) continue;
-
-                if (teleport.containsKey(next)) {
-                    next = teleport.get(next);
+            for (int i = 1; i <= 6; i++) {
+                int next = index + i;
+                if (next > FINISH) {
+                    continue;
                 }
-
+                next = teleport.getOrDefault(next, next);
+                
                 if (!visited[next]) {
                     visited[next] = true;
                     queue.add(new int[]{next, count + 1});
