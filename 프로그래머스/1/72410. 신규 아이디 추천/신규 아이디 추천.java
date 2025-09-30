6336e03549ba49633cf7cb54722a9afa983a4b6e
SungEun Kim <81363864+sung-silver@users.noreply.github.com>
2025-09-30 17:47:22 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.04 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,53 +1,54 @@
-import java.util.*;
-
 class Solution {
     public String solution(String new_id) {
         String answer = "";
-        String recommend_id = new_id.toLowerCase();
         StringBuilder sb = new StringBuilder();
         
-        for(int i=0 ; i<recommend_id.length() ; i++) {
-            char now = recommend_id.charAt(i);
-            
-            if(Character.isLetterOrDigit(now) || now == '_' || now == '-' || now == '.') {
-                if(sb.length() > 0) {
-                    char before = sb.charAt(sb.length()-1);
-                    if(now == '.' && before == '.') {
+        // 소문자, 허용되는 문자만 추가 이때, 마침표 치환도 추가
+        for(int i = 0 ; i < new_id.length() ; i++) {
+            char c = new_id.charAt(i);
+            if(Character.isDigit(c)) {
+                sb.append(c);
+            } else if(Character.isLetter(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+            else if( c == '.' ||  c == '-' || c == '_') {
+                if(i != 0 && sb.length() > 0) {
+                    char before = sb.charAt(sb.length() - 1);
+                    if(before == '.' && c == '.') {
                         continue;
-                    } 
+                    }
                 }
-                sb.append(now);
-            }  
+                sb.append(c);
+            }
         }
         
+        // 맨 앞뒤 문자가 마침표라면 제거
         if (sb.length() > 0 && sb.charAt(0) == '.') {
             sb.deleteCharAt(0);
         }
-        
+
         if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '.') {
             sb.deleteCharAt(sb.length() - 1);
         }
         
-        if(sb.length() == 0) {
+        if (sb.length() == 0) {
             sb.append('a');
         }
         
-        
         if(sb.length() > 15) {
-            if(sb.charAt(14) == '.') {
-                sb.setLength(14);
-            } else {
-                sb.setLength(15);
+            sb.setLength(15);
+
+            if (sb.charAt(sb.length() - 1) == '.') {
+                sb.deleteCharAt(sb.length() - 1);
             }
         }
         
-
-        while(sb.length() < 3) {
-            sb.append(sb.charAt(sb.length()-1));
+        while(sb.length() <= 2) {
+            sb.append(sb.charAt(sb.length() - 1));
         }
-
         
         answer = sb.toString();
+
         return answer;
     }
 }
