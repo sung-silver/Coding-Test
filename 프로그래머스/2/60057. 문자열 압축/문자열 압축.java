c9af2493a278e569aef92b15cd2f703b8dfe38dd
SungEun Kim <81363864+sung-silver@users.noreply.github.com>
2025-09-30 16:31:05 +0900

    [level 2] Title: 문자열 압축, Time: 16.67 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -4,40 +4,27 @@ class Solution {
     public int solution(String s) {
         int answer = s.length();
         
-        for(int step = 1 ; step < s.length() ; step++) {
-            String key = "";
-            int len = 0;
-            int count = 1;
+        for (int step = 1; step <= s.length(); step++) {
+            StringBuilder sb = new StringBuilder();
             String compare = s.substring(0, step);
-            len += step;
+            int count = 1;
             
-            while(len+step <= s.length()) {
-                String current = s.substring(len, len+step);
-                if(current.equals(compare)) {
+            for (int i = step; i < s.length(); i += step) {
+                int end = Math.min(i + step, s.length());
+                String current = s.substring(i, end);
+                
+                if (current.equals(compare)) {
                     count++;
                 } else {
-                    if(count == 1) {
-                        key += compare;
-                    } else {
-                        key += count + compare;
-                    }
-                    count = 1;
+                    sb.append(count == 1 ? "" : count).append(compare);
                     compare = current;
+                    count = 1;
                 }
-                len+=step;
             }
             
-            if(count == 1) {
-                key += compare;
-            } else {
-                key += count + compare;
-            }
-        
+            sb.append(count == 1 ? "" : count).append(compare);
             
-            if(len < s.length()) {
-                key += s.substring(len);
-            }
-            answer = Math.min(answer, key.length());
+            answer = Math.min(answer, sb.length());
         }
         
         return answer;
